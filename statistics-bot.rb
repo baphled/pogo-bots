03373c3625794e564908d3ef960f8a8bfe3d7a68
Yomi Colledge <baphled@boodah.net>
2017-12-05 02:39:51 +0000

    Small tweaks

--- statistics-bot.rb
+++ statistics-bot.rb
@@ -133,9 +133,6 @@ bot.message(start_with: 'compare!') do |event|
 
     leader, runner_up = [my_stats, their_stats].sort { |a,b| b[:total_xp] <=> a[:total_xp] }
 
-    leader[:total_xp] = leader[:total_xp].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-    runner_up[:total_xp] = runner_up[:total_xp].to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-
     message << print_compare_players(leader, runner_up, :total_xp, 'XP')
 
     leader, runner_up = [my_stats, their_stats].sort { |a,b| b[:total_gyms] <=> a[:total_gyms] }
@@ -179,7 +176,7 @@ bot.message(content: 'top-team!') do |event|
 
 
   ordered.each_with_index do |team, index|
-    message << "#{index + 1}. #{team.keys.first.capitalize} (#{team.values.first.to_s.reverse.gsub(/(\d{4})(?=\d)/, '\\1,').reverse} XP)\n"
+    message << "#{index + 1}. #{team.values.first.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse} XP (#{team.keys.first.capitalize})\n"
   end
 
   event.respond message
