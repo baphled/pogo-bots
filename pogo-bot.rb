9cc0fadbede953d6eec563acb7281ed0d8b96bd3
Yomi Colledge <baphled@boodah.net>
2017-12-28 10:54:16 +0000

    Move !links to the PoGo bot

--- pogo-bot.rb
+++ pogo-bot.rb
@@ -11,6 +11,16 @@ require_relative './lib/po_go_helper'
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['POGO_DISCORD_TOKEN'], prefix: '!'
 
+bot.command(:links, description: 'Display helpful links') do |event|
+  links = """
+Pogo Stats: https://docs.google.com/spreadsheets/d/1ZLXHU0FU-_ejkxP_Z_19iEv5FBWDdJ_rQargbKh7qGY/edit?usp=sharing
+  """
+
+  event.respond links
+
+  nil
+end
+
 bot.command(:'next-migration', description: 'Displays when the next migration will be') do |event|
   event.respond "The next nest migration is: #{PoGoHelper.next_migration}"
 
--- statistics-bot.rb
+++ statistics-bot.rb
@@ -9,16 +9,6 @@ require_relative './lib/pogo_stats'
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['STATS_DISCORD_TOKEN'], prefix: '!'
 
-bot.command(:links, description: 'Display helpful links') do |event|
-  links = """
-Pogo Stats: https://docs.google.com/spreadsheets/d/1ZLXHU0FU-_ejkxP_Z_19iEv5FBWDdJ_rQargbKh7qGY/edit?usp=sharing
-  """
-
-  event.respond links
-
-  nil
-end
-
 bot.command(:top, min_args: 1, max_args: 2, description: 'Display the top trainers based on a given stat') do |event, limiter, selector_type|
   limiter = limiter.to_i
 
