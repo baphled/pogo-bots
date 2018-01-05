078b3587a291fbfb9515c128baadd4d777bd8505
Yomi Colledge <baphled@boodah.net>
2018-01-05 04:36:52 +0000

    Update Mystic colours

--- lib/team_colour_matrix/mystic.rb
+++ lib/team_colour_matrix/mystic.rb
@@ -2,13 +2,14 @@ module TeamColourMatrix
   class Mystic < TeamColourMatrix::Base
     def self.colours
       [
-        [0,42,84],
+        [0, 42, 84],
         [0, 71, 142],
         [0, 71, 143],
         [0, 119, 237],
         [0, 119, 238],
-        [1,120,238],
-        [2,116,241],
+        [1, 120, 238],
+        [2, 116, 241],
+        [50, 114, 233],
       ]
     end
   end
