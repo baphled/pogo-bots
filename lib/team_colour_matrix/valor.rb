a3564f7cd730303c0cd81769f18adaca36f9aa03
Yomi Colledge <baphled@boodah.net>
2018-01-05 04:40:14 +0000

    Comment ranges of Red mapped to Valor player images

--- lib/team_colour_matrix/valor.rb
+++ lib/team_colour_matrix/valor.rb
@@ -2,12 +2,12 @@ module TeamColourMatrix
   class Valor < TeamColourMatrix::Base
     def self.colours
       [
-        [89, 0, 17],
-        [90, 0, 18],
-        [151, 1, 30],
-        [153, 0, 30],
-        [255, 9, 49],
-        [234, 53, 60],
+        [89, 0, 17],   # Dark Red
+        [90, 0, 18],   # Dark Red
+        [151, 1, 30],  # Orangey Red
+        [153, 0, 30],  # Tomato Red
+        [255, 9, 49],  # Bright Red
+        [234, 53, 60], # Bright Red
       ]
     end
   end
