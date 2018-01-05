ae91a28594feea300e20ace00ca73e33fefde6a3
Yomi Colledge <baphled@boodah.net>
2018-01-05 06:13:18 +0000

    Update Instinct RGB array

--- lib/team_colour_matrix/instinct.rb
+++ lib/team_colour_matrix/instinct.rb
@@ -2,10 +2,11 @@ module TeamColourMatrix
   class Instinct < TeamColourMatrix::Base
     def self.colours
       [
-        [255, 200, 5],
-        [246, 202, 69],
-        [153, 122, 0],
+        [90, 72, 0],
         [91, 73, 0],
+        [153, 122, 0],
+        [246, 202, 69],
+        [255, 200, 5],
       ]
     end
   end
